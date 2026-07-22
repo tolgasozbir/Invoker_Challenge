8dfc828d0b6358ccff8d0ad6262127b9b6e2ae5e
Tolga Sözbir <tolgasoz1@gmail.com>
2026-07-22 23:49:21 +0300

    chore(1.9.0 hotfix): target Android 16 (API 36) for Play compliance

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         // 🔽 AdMob placeholder
